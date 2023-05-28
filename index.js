77eb1335fb2b056f136a94fe32514cbfb456b316
maks <maks.marolt@gmail.com>
2023-05-28 12:05:31 +0200

    1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const isEmpty = require('lodash.isempty');
 app.use(express.json());
 
